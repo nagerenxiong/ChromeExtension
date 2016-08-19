0437258757f4847b81b20b141bc22219b9cb454f
nagerenxiong <527935649@qq.com>
2016-08-19 14:12:51 +0800

    微信

--- 微信加群/popup.js
+++ 微信加群/popup.js
@@ -1,5 +1,10 @@
 // rome.tabs.executeScript(integer tabId, tabs.InjectDetails details, function callback)
 $("#queding").click(function() {
-	chrome.extension.getBackgroundPage().start();
+	chrome.extension.getBackgroundPage().start($.trim($("#name").val()),$.trim($("#pass").val()),function(res,callback){
+		if(res=="success")
+			callback();
+		else if(res=="error")
+			alert("请联系tb003095_2013购买会员");
+	});
 	// alert("启动成功！不用再次点击");
 })
