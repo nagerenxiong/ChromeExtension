50c600af004fa16f50d32347d0a59c013c646e7c
nagerenxiong <527935649@qq.com>
2016-08-23 11:51:35 +0800

    微信加群更新

--- 微信加群/popup.js
+++ 微信加群/popup.js
@@ -1,10 +1,27 @@
 // rome.tabs.executeScript(integer tabId, tabs.InjectDetails details, function callback)
+if (chrome.extension.getBackgroundPage().fag) {
+	$("ul li:lt(2)").hide();
+} else {
+	$("ul li:lt(2)").show();
+}
 $("#queding").click(function() {
-	chrome.extension.getBackgroundPage().start($.trim($("#name").val()),$.trim($("#pass").val()),function(res,callback){
-		if(res=="success")
+	if (chrome.extension.getBackgroundPage().fag) {
+		chrome.extension.getBackgroundPage().query();
+	}
+	else
+	{
+		login();
+	}
+})
+
+function login() {
+	chrome.extension.getBackgroundPage().start($.trim($("#name").val()), $.trim($("#pass").val()), function(res, callback) {
+		if (res == "success") {
 			callback();
-		else if(res=="error")
-			alert("请联系tb003095_2013购买会员");
+			$("ul li:lt(2)").hide();
+		} else if (res == "error") {
+			alert("请联系QQ1274123708购买会员");
+		}
 	});
 	// alert("启动成功！不用再次点击");
-})
+}
