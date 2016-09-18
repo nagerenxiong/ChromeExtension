d8d2c52e2349ff38ab4e080e19209f91dfbae90d
nagerenxiong <527935649@qq.com>
2016-09-18 15:00:24 +0800

    0918

--- 京东/content.js
+++ 京东/content.js
@@ -5,10 +5,16 @@ chrome.runtime.sendMessage({
   console.log(response);
   if (response == "error") {
     $("#summary-price .dd").append(
-      "<span style='font-size:16px;color:red;font-weight:bold;margin-left:30px;'>佣金:请登陆联盟"
+      "<span style='font-size:16px;color:red;font-weight:bold;margin-left:30px;'>佣金:<a href='https://media.jd.com/' target='_blank' style='color:red' >请登陆联盟</a>"
+    );
+    $(".summary-price .dd").append(
+      "<span style='font-size:16px;color:red;font-weight:bold;margin-left:30px;'>佣金:<a href='https://media.jd.com/' target='_blank' style='color:red'>请登陆联盟</a>"
     );
   } else if (response == "无") {
     $("#summary-price .dd").append(
+      "<span style='font-size:16px;color:red;font-weight:bold;margin-left:30px;'>佣金:无"
+    );
+    $(".summary-price .dd").append(
       "<span style='font-size:16px;color:red;font-weight:bold;margin-left:30px;'>佣金:无"
     );
   } else {
