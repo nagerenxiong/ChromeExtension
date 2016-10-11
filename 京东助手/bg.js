8b52e74ea3276b15896ca62f982fa00b52700965
nagerenxiong <527935649@qq.com>
2016-10-12 02:33:04 +0800

    1012

--- 京东助手/bg.js
+++ 京东助手/bg.js
@@ -1,50 +1,48 @@
-var index = 0;
-var fag = false;
-
-function start(id, callback) {
+function start(url, callback) {
 	$.ajax({
-			url: 'https://media.jd.com/gotoadv/goods?pageIndex=&pageSize=10&property=&sort=&adownerType=&pcRate=&wlRate=&category=&category1=0&condition=0&fromPrice=&toPrice=&keyword=' +
-				id,
+			url: url,
 			type: 'get',
-			async: false
+			async: false,
+			dataType: "json"
 		})
 		.done(function(data) {
 			callback(data);
 		})
 		.fail(function(msg) {
-			callback("error");
+			callback(msg);
 		})
 }
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 	if (request.type == 1) {
 		var id = request.id;
-		var url = request.url;
-		$("#frame").attr("src", url)
-		start(id, function(data) {
-			if (data == "error") {
-				sendResponse("error");
+		var url = 'http://dr.jd.com/articleJf/getSku.do?skuId=' + id;
+		start(url, function(data) {
+			if (data == null || !data.pcCommissionShare) {
+				sendResponse("null");
 				return;
 			}
-			if (data.indexOf("没有查询到符合条件的数据") >= 0) {
-				sendResponse("无");
-				return;
-			}
-			var p1 = data.indexOf("无线：");
-			var p2 = data.indexOf("无线：", p1 + 2);
-			var d2 = data.indexOf("</td>", p2);
-			var p3 = data.indexOf("无线：", p2 + 2);
-			var d3 = data.indexOf("</td>", p3);
-			var k1 = data.substring(p2, d2);
-			var k2 = data.substring(p3, d3);
-			var pk2 = k2.indexOf("￥");
-			var v2 = $.trim(k2.substr(pk2));
-			var pk1 = k1.indexOf("：");
-			var v1 = $.trim(k1.substr(pk1 + 1));
-			sendResponse({
-				v1: v1,
-				v2: v2
-			});
+			var yjl = data.pcCommissionShare;
+			var url1 =
+				"https://sclub.jd.com/comment/productPageComments.action?productId=" +
+				id +
+				"&score=0&sortType=3&page=0&pageSize=10&callback=fetchJSON_comment98vv5739";
+			start(url1, function(msg) {
+				var data1 = msg.responseText;
+				var i1 = data1.indexOf("goodRateShow");
+				var i2 = data1.indexOf(":", i1) + 1;
+				var i3 = data1.indexOf(",", i2);
+				var j1 = data1.indexOf("poorCount");
+				var j2 = data1.indexOf(":", j1) + 1;
+				var j3 = data1.indexOf(",", j2);
+				var hpl = data1.substring(i2, i3) + "%";
+				var cps = data1.substring(j2, j3).replace(/\"/g, "");
+				sendResponse({
+					yjl: yjl,
+					hpl: hpl,
+					cps: cps
+				});
+			})
 		});
 	} else if (request.type == 2) {
 		$.ajax({
@@ -67,62 +65,5 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 			.fail(function(msg) {
 				sendResponse("error");
 			})
-	} else if (request.type == 3) {
-		var commentUrl = request.commentUrl;
-		$.ajax({
-				url: commentUrl,
-				type: 'get',
-				dataType: "string",
-				async: false
-			})
-			.done(function(data) {
-				console.log(data);
-			})
-			.fail(function(msg) {
-				var data = msg.responseText;
-				var i1 = data.indexOf("goodRateShow");
-				var i2 = data.indexOf(":", i1) + 1;
-				var i3 = data.indexOf(",", i2);
-				var j1 = data.indexOf("poorCount");
-				var j2 = data.indexOf(":", j1) + 1;
-				var j3 = data.indexOf(",", j2);
-				var hpl = data.substring(i2, i3) + "%";
-				var cps = data.substring(j2, j3);
-				sendResponse({
-					hpl: hpl,
-					cps: cps
-				});
-			})
 	}
 })
-chrome.webRequest.onBeforeRequest.addListener(
-	function(details) {
-		// if (details.url.indexOf("conversion_async.js") >= 0 && details.frameId != 0) {
-		// 	console.log(details);
-		// 	return {
-		// 		redirectUrl: "http://1.nageren.sinaapp.com/temp1.js"
-		// 	};
-		// } else
-		if (details.url.indexOf("fetchJSON_comment") >= 0 && details.type ==
-			"script") {
-			console.log(details);
-			chrome.tabs.query({
-				active: true,
-				currentWindow: true
-			}, function(tabs) {
-				console.log(tabs);
-				chrome.tabs.sendMessage(tabs[0].id, {
-					commentUrl: details.url
-				});
-			});
-			return {};
-		}
-	}, {
-		urls: [
-			"<all_urls>"
-		]
-	}, ["blocking"]);
-
-
-
-// http://sclub.jd.com/productpage/p-2916342-s-0-t-3-p-0.html?callback=fetchJSON_comment98vv6043
--- 京东助手/content.js
+++ 京东助手/content.js
@@ -2,80 +2,66 @@ var url = window.location.href;
 var xx = url.lastIndexOf(".html");
 var pp = url.lastIndexOf("/");
 var id = url.substring(pp + 1, xx);
-var hpd = $(".rate strong").text();
 window.onload = function() {
   chrome.runtime.sendMessage({
     type: 1,
-    id: id,
-    url: url + "#comment"
+    id: id
   }, function(response) {
-    if (response == "error") {
+    console.log(response);
+    if (response == "null") {
       $("#summary-price .dd").append(
-        "<span style='font-size:16px;color:red;font-weight:bold;margin-left:30px;'>佣金:<a href='https://media.jd.com/' target='_blank' style='color:red' >点击登录联盟</a>"
+        "<span style='font-size:16px;color:red;font-weight:bold;margin-left:30px;'>佣金:无(检查当前账号是否登陆或者注册达人精选)"
       );
       $(".summary-price .dd").append(
-        "<span style='font-size:16px;color:red;font-weight:bold;margin-left:30px;'>佣金:<a href='https://media.jd.com/' target='_blank' style='color:red'>点击登录联盟</a>"
-      );
-    } else if (response == "无") {
-      $("#summary-price .dd").append(
-        "<span style='font-size:16px;color:red;font-weight:bold;margin-left:30px;'>佣金:无"
-      );
-      $(".summary-price .dd").append(
-        "<span style='font-size:16px;color:red;font-weight:bold;margin-left:30px;'>佣金:无"
+        "<span style='font-size:16px;color:red;font-weight:bold;margin-left:30px;'>佣金:无(检查当前账号是否登陆或者注册达人精选)"
       );
     } else {
+      var price = $(".J-p-" + id).eq(0).text().replace("￥", "");
+      var yjl = response.yjl.replace("%", "");
+      if ($("#jd-price").length > 0) {
+        price = $("#jd-price").text();
+      }
+      var yj = parseFloat(yjl) * parseFloat(price) / 100;
+      console.log(price);
       $("#summary-price .dd").append(
         "<span style='font-size:16px;color:red;font-weight:bold;margin-left:30px;'>佣金:" +
-        response.v2 + "(" + response
-        .v1 +
-        ")" + hpd +
+        parseFloat(yj.toFixed(2)) + "(" + response
+        .yjl +
+        ") 好评率：" + response.hpl + " 差评数:" + response.cps +
         "</span><span class='hpl_hhhh' style='font-size:16px;color:red;font-weight:bold;margin-left:10px;'></span><a class='postToServerBtn' data-v2='" +
         response.v2 +
         "' data-v1='" + response.v1 +
-        "'  href='javascript:void(0)' style='font-size:16px;color:red;font-weight:bold;margin-left:10px;display:none'>提交</a>"
+        "'  href='javascript:void(0)' style='font-size:16px;color:red;font-weight:bold;margin-left:10px;display:none'>提交</a><br>插件反馈群：426387850"
       );
       $(".summary-price .dd").append(
         "<span style='font-size:16px;color:red;font-weight:bold;margin-left:30px;'>佣金:" +
-        response.v2 + "(" + response
-        .v1 +
-        ")" + hpd +
+        parseFloat(yj.toFixed(2)) + "(" + response
+        .yjl +
+        ") 好评率：" + response.hpl + " 差评数:" + response.cps +
         "</span><span class='hpl_hhhh' style='font-size:16px;color:red;font-weight:bold;margin-left:10px;'></span><a class='postToServerBtn' data-v2='" +
         response.v2 +
         "' data-v1='" + response.v1 +
-        "' href='javascript:void(0)' style='font-size:16px;color:red;font-weight:bold;margin-left:10px;display:none'>提交</a>"
+        "' href='javascript:void(0)' style='font-size:16px;color:red;font-weight:bold;margin-left:10px;display:none'>提交</a><br>插件反馈群：426387850"
       );
     }
   })
+  $(".file").remove();
+  $(".file_explain").css("position", "relative").parent().append(
+    "<div id='checkBox' style='position:absolute;left:200px;height:100%;top:0'></div>"
+  );
+  $("#sku").on('blur', function() {
+    setTimeout(function() {
+      $("#checkBox").html($(".pics").clone(true));
+      $("#checkBox .pic").removeClass("on");
+      $("#checkBox .pic").click(function() {
+        $(".file_view img").css("display", "inline").attr("src",
+          $(this)
+          .children("img").attr("src"));
+      })
+    }, 1000)
+  })
 }
 
-chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    console.log(request.commentUrl);
-    var commentUrl = request.commentUrl;
-    chrome.runtime.sendMessage({
-      commentUrl: commentUrl,
-      type: 3
-    }, function(response) {
-      $(".hpl_hhhh").text("好评率：" + response.hpl + "  差评数:" + response.cps);
-    })
-  });
-
-// function fetchTwitterFeed(url, callback) {
-//   var xhr = new XMLHttpRequest();
-//   xhr.onreadystatechange = function(data) {
-//     if (xhr.readyState == 4) {
-//       if (xhr.status == 200) {
-//         // var data = JSON.parse(xhr.responseText);
-//         callback(data);
-//       } else {
-//         callback(null);
-//       }
-//     }
-//   }
-//   var url = url;
-//   xhr.open('GET', url, true);
-//   xhr.send();
-// };
 
 $(document).on('click', '.postToServerBtn', function() {
   var commission = $(this).attr("data-v2").replace('￥', '');
